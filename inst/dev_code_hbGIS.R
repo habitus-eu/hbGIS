365f3e4e28defbbf1feab0a90ef4b1fbb47ab807
Vincent van Hees <v.vanhees@accelting.com>
2023-11-22 15:06:41 +0100

    Update dev_code_hbGIS.R

--- inst/dev_code_hbGIS.R
+++ inst/dev_code_hbGIS.R
@@ -17,13 +17,22 @@ library(purrr)
 library(geosphere)
 library(lwgeom)
 
-hbGIS(gisdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata/GIS",
-                 palmsdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata/hbGPSoutput",
-                 gislinkfile = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata/Tables/participant_basis.csv",
-                 outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata",
-                 dataset_name = "BEtestdata",
-                 configfile = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata/config_palmsplusr.csv")
+hbGIS(gisdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/NBBB2010/GIS",
+                 palmsdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/NBBB2010/hbGPSoutput",
+                 gislinkfile = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/NBBB2010/Tables/participant_basis.csv",
+                 outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/NBBB2010",
+                 dataset_name = "NBBB2010",
+                 configfile = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/NBBB2010/config_palmsplusr.csv")
 kkk
+
+# Belgium dataset
+# hbGIS(gisdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata/GIS",
+#       palmsdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata/hbGPSoutput",
+#       gislinkfile = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata/Tables/participant_basis.csv",
+#       outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata",
+#       dataset_name = "BEtestdata",
+#       configfile = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/BEtestdata/config_palmsplusr.csv")
+
 # hbGIS(gisdir = "/media/vincent/projects/Habitus/palmsplusr/testdata/GIS",
 #                  palmsdir = "/media/vincent/projects/Habitus/palmsplusr/testdata/PALMS_output/",
 #                  gislinkfile = "/media/vincent/projects/Habitus/palmsplusr/testdata/Tables/participant_basis.csv",
